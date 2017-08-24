0ccc12acddcb6a1217bb58049d65d9214b2b144e
Lookis <lookisliu@gmail.com>
2017-08-25 06:40:18 +0800

    mock redis for test

--- src/redis.js
+++ src/redis.js
@@ -1,12 +1,22 @@
-import redis from 'redis';
+/* eslint-disable global-require,import/no-extraneous-dependencies */
 import config from 'config';
 import bluebird from 'bluebird';
+import redis from 'redis';
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-const client = redis.createClient(config.redis);
-const subscriber = redis.createClient(config.redis);
+let client;
+let subscriber;
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+  client = redis.createClient(config.redis);
+  subscriber = redis.createClient(config.redis);
+} else {
+  client = require('redis-js');
+  subscriber = require('redis-js');
+  bluebird.promisifyAll(client);
+  bluebird.promisifyAll(subscriber);
+}
 
 client.on('error', err => console.log(err)); // eslint-disable-line no-console
 subscriber.on('error', err => console.log(err)); // eslint-disable-line no-console
